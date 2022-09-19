b965ec11d6f8a398b286252e06d5739b694be0cb
ristafn <golemmite23@gmail.com>
2022-09-19 08:54:56 +0700

    feat: add stateful

--- lib/features/stateful_state/views/stateful_page.dart
+++ lib/features/stateful_state/views/stateful_page.dart
@@ -8,8 +8,6 @@ class StatefulPage extends StatefulWidget {
 }
 
 class _StatefulPageState extends State<StatefulPage> {
-  final TextEditingController _editingController = TextEditingController();
-
   @override
   void initState() {
     super.initState();
@@ -46,13 +44,86 @@ class _StatefulPageState extends State<StatefulPage> {
     print('Do the deactive');
   }
 
+  double _onSlide = 0;
+  bool _isSwitched = false;
+  bool _isChecked = false;
+
   @override
   Widget build(BuildContext context) {
+    const TextStyle titleStyle = TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Stateful'),
       ),
-      body: Container(),
+      body: Container(
+        margin: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Slider',
+              style: titleStyle,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Slider(
+              value: _onSlide,
+              onChanged: (value) {
+                setState(() {
+                  _onSlide = value;
+                });
+              },
+              divisions: 5,
+              activeColor: Colors.green,
+              thumbColor: Colors.amber,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Switch',
+              style: titleStyle,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Switch(
+              value: _isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  _isSwitched = value;
+                });
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Checkbox',
+              style: titleStyle,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Checkbox(
+              value: _isChecked,
+              onChanged: (value) {
+                setState(() {
+                  _isChecked = value!;
+                });
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
