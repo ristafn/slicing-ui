e256cdc242a489cbf7ac6e956de90f8ae0eaab1a
ristafn <golemmite23@gmail.com>
2022-09-20 13:37:00 +0700

    feat: adaptive and responsive layout

---
+++ lib/features/adaptive_layout/views/adaptive_layout_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayoutPage extends StatelessWidget {
+  const AdaptiveLayoutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
